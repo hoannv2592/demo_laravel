58ad5f405195e19e5a2a8bddbdf3c2c0020faba3
hoannv2592 <vanhoanpt92@gmail.com>
2018-01-09 17:35:09 +0700

    login need change

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login','LoginController@getLogin');
+Route::post('login','LoginController@postLogin');
+Route::get('','HomeController@getIndex');
+Route::get('index','HomeController@indexshow');
